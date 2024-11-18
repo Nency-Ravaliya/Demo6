75f23a67316a6a509383b1503261198475adebb8
Nency-Ravaliya <ravaliyanensi@gmail.com>
2024-11-18 20:36:48 +0530

    ok

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+// Smooth scroll to contact section when the "Get in Touch" button is clicked
+document.getElementById('contact-btn').addEventListener('click', function() {
+    document.getElementById('contact-section').scrollIntoView({behavior: 'smooth'});
+});
